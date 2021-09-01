ed7f293cca7d561bbd899f8a8694fc551def1baf
hugocostav <hugocostam100@outlook.com.br>
2021-09-01 18:08:42 -0300

    addef first ejs template | connection with local database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var conn = require("./../link/db");
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  conn.query("SELECT * FROM tb_menus ORDER BY title", (err, results) => {
+    if (err) {
+      console.log(err);
+    }
+    res.render("index", {
+      title: "RauranteRest Saboroso!",
+      menus: results,
+    });
+  });
 });
 
 module.exports = router;
